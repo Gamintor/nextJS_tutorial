a231ff85ffb21be6b35108f0da50821562f99c71
Gamintor <relkovic10@gmail.com>
2022-04-14 14:09:18 +0200

    added getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import path from 'path';
+import fs from 'fs/promises';
 
-function HomePage() {
+function HomePage(props) {
+	const { products } = props;
+
 	return (
 		<ul>
-			<li>Product 1</li>
-			<li>Product 2</li>
-			<li>Product 3</li>
+			{products.map(item => (
+				<li key={item.id}>{item.title}</li>
+			))}
 		</ul>
 	);
+}
+
+export async function getStaticProps() {
+	const filePath = path.join(process.cwd(), 'dummy-backend.json');
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+
+	return {
+		props: {
+			products: data.products,
+		},
+	};
 }
 
 export default HomePage;
