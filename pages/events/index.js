5be22d369de01d4b39a07f6ed99d9f522c83514d
Gamintor <relkovic10@gmail.com>
2022-04-14 09:42:59 +0200

    final version

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,13 +3,19 @@ import EventList from '../../components/events/eventList';
 import { getAllEvents } from '../../dummy-data';
 import EventsSearch from '../../components/events/EventsSearch';
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 function Events() {
+	const router = useRouter();
 	const events = getAllEvents();
+
+	function findEventsHandler(year, month) {
+		router.push(`/events/${year}/${month}`);
+	}
 
 	return (
 		<Fragment>
-			<EventsSearch />
+			<EventsSearch onSearch={findEventsHandler} />
 			<EventList items={events} />
 		</Fragment>
 	);
