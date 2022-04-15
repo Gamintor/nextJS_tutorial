91b12a443096e757aeb1e1b7d6902be74151646c
Gamintor <relkovic10@gmail.com>
2022-04-15 11:11:04 +0200

    serversideprops added

--- pages/[pid].js
+++ pages/[pid].js
@@ -18,14 +18,21 @@ function ProductDetailPage(props) {
 	);
 }
 
-export async function getStaticProps(context) {
-	const { params } = context;
-	const productId = params.pid;
+async function getData() {
 	const filePath = path.join(process.cwd(), 'dummy-backend.json');
 	const jsonData = await fs.readFile(filePath);
 	const data = JSON.parse(jsonData);
+	return data;
+}
 
+export async function getStaticProps(context) {
+	const { params } = context;
+	const productId = params.pid;
+	const data = await getData();
 	const product = data.products.find(e => e.id === productId);
+	if (!product) {
+		return { notFound: true };
+	}
 
 	return {
 		props: {
@@ -35,11 +42,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+	const data = await getData();
+	const ids = data.products.map(product => product.id);
+	const pathsWithParams = ids.map(id => ({ params: { pid: id } }));
+
 	return {
-		paths: [{ params: { pid: 'p1' } }],
+		paths: pathsWithParams,
 		fallback: true,
 	};
 }
 
 export default ProductDetailPage;
-1;
