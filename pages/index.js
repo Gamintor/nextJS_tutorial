bb124450cb2e420b6fca43a03ee681ecaac1022c
Gamintor <relkovic10@gmail.com>
2022-04-20 08:36:36 +0200

    intial setup

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import Head from 'next/head';
-import EventList from '../components/events/EventList';
-import { getFeaturedEvents } from '../helpers/api-util';
 
-function HomePage(props) {
+function HomePage() {
 	return (
 		<div>
-			<Head>
-				<title>NextJS events</title>
-				<meta name='description' content='Find a lot of great events that will make you grow!' />
-			</Head>
-			<EventList items={props.events} />
+			<h1>The Home Page</h1>
 		</div>
 	);
-}
-
-export async function getStaticProps() {
-	const featuredEvents = await getFeaturedEvents();
-
-	return {
-		props: {
-			events: featuredEvents,
-		},
-		revalidate: 1800,
-	};
 }
 
 export default HomePage;
