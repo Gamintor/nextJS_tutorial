b12042c47a677aba937696aa59c93b73183237a1
Gamintor <relkovic10@gmail.com>
2022-04-20 09:40:41 +0200

    added some backend

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useRef } from 'react';
 
 function HomePage() {
+	const emailInputRef = useRef();
+	const feedbackRef = useRef();
+
+	function submitEventHandler(e) {
+		e.preventDefault();
+		const enteredMail = emailInputRef.current.value;
+		const enteredFeedback = feedbackRef.current.value;
+		
+	}
+
 	return (
 		<div>
 			<h1>The Home Page</h1>
+			<form>
+				<div>
+					<label htmlFor='email'>Your email adrress</label>
+					<input type='email' id='email' ref={emailInputRef} />
+				</div>
+				<div>
+					<label htmlFor='feedback'>Your feedback</label>
+					<textarea id='feedback' rows='5' ref={feedbackRef} />
+				</div>
+				<button>Send feedback</button>
+			</form>
 		</div>
 	);
 }
