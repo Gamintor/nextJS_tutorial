17a52c66f6fde4de159cc104d126e4b3f2579b44
Gamintor <relkovic10@gmail.com>
2022-04-25 10:47:38 +0200

    update

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,32 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
-function SinglePostPage() {
-	return <PostContent />;
+function SinglePostPage(props) {
+	return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+	const { params } = context;
+	const { slug } = params;
+	const postData = getPostData(slug);
+
+	return {
+		props: {
+			post: postData,
+		},
+		revalidate: 600,
+	};
+}
+
+export function getStaticPaths() {
+	const postFilenames = getPostsFiles();
+
+	const slugs = postFilenames.map(item => item.replace(/\.md$/, ''));
+
+	return {
+		paths: slugs.map(slug => ({ params: { slug: slug } })),
+		fallback: false,
+	};
 }
 
 export default SinglePostPage;
